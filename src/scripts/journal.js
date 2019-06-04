bc57f88d4720bedc0fbe8a26cd09a1d5f7324c79
joekennerly <joseph.kennerly@gmail.com>
2019-06-03 21:49:29 -0500

    add content to journal entry objects

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,23 +2,23 @@
 // ─── DEFINE OBJECTS THAT REPRESENT JOURNAL ENTRIES AND ADD THEM TO AN ARRAY ─────
 //
 
-let journal = [
+let journalEntries = [
   {
-    date: "",
-    concepts: "",
-    entry: "",
-    mood: ""
+    date: "05/20/2019",
+    concepts: "How to learn",
+    entry: "Great intro to the course!",
+    mood: "Extatic"
   },
   {
-    date: "",
-    concepts: "",
-    entry: "",
-    mood: ""
+    date: "05/21/2019",
+    concepts: "git",
+    entry: "We learned git",
+    mood: "confused"
   },
   {
-    date: "",
-    concepts: "",
-    entry: "",
-    mood: ""
+    date: "05/22/2019",
+    concepts: "html css",
+    entry: "I'm on my way!",
+    mood: "confident"
   }
 ]

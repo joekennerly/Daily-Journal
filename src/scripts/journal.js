34ae0a262c801187a322fee65c783d6a20db7b4a
joekennerly <joseph.kennerly@gmail.com>
2019-06-04 21:38:08 -0500

    build template and push to array

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,24 +1,19 @@
-//
-// ─── DEFINE OBJECTS THAT REPRESENT JOURNAL ENTRIES AND ADD THEM TO AN ARRAY ─────
-//
+// Once you define that variable and give it a default value of a blank array, use the push() method to add the journalEntry object you defined to the array.
 
-let journalEntries = [
-  {
-    date: "05/20/2019",
-    concepts: "How to learn",
-    entry: "Great intro to the course!",
-    mood: "Extatic"
-  },
-  {
-    date: "05/21/2019",
-    concepts: "git",
-    entry: "We learned git",
-    mood: "confused"
-  },
-  {
-    date: "05/22/2019",
-    concepts: "html css",
-    entry: "I'm on my way!",
-    mood: "confident"
-  }
-]
+// Create a container
+const allEntries = []
+console.log(allEntries);
+
+// Create a template
+const journalEntryObject = {
+  date:"march",
+  title:"title",
+  entry:"this is entry",
+  mood:"mood"
+}
+console.log(journalEntryObject);
+
+// Push the template literal
+allEntries.push(`${journalEntryObject}`)
+
+console.log(allEntries)
